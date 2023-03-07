6a64f5a8ef4541f9f75b2414278f24232ba7fa6a
Andreas Fuchs <info@anfuchs.de>
2023-03-07 22:49:51 +0100

    adds data-request via auth to progress-updater

--- Assets/Stable-Diffusion-Unity-Integration/Scripts/StableDiffusionGenerator.cs
+++ Assets/Stable-Diffusion-Unity-Integration/Scripts/StableDiffusionGenerator.cs
@@ -1,14 +1,18 @@
+using System;
 using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
+using System.Text;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class StableDiffusionGenerator : MonoBehaviour
 {
     static protected StableDiffusionConfiguration sdc = null;
-
+    private Coroutine _updateProgressRunning = null;
+    
     /// <summary>
     /// Update a generation progress bar
     /// </summary>
@@ -34,6 +38,42 @@ public class StableDiffusionGenerator : MonoBehaviour
 #endif
     }
 
+    /// <summary>
+    /// Update a generation progress bar with auth
+    /// </summary>
+    protected void UpdateGenerationProgressWithAuth()
+    {
+        #if UNITY_EDITOR
+            if (_updateProgressRunning != null) return;
+            _updateProgressRunning = StartCoroutine(UpdateGenerationProgressWithAuthCor());
+        #endif
+    }
+    
+    private IEnumerator UpdateGenerationProgressWithAuthCor()
+    {
+        // Stable diffusion API url for setting a model
+        string url = sdc.settings.StableDiffusionServerURL + sdc.settings.ProgressAPI;
+        float progress = 0;
+
+        using (UnityWebRequest modelInfoRequest = UnityWebRequest.Get(url))
+        {
+            byte[] bytesToEncode = Encoding.UTF8.GetBytes(sdc.settings.user + ":" + sdc.settings.pass);
+            string encodedText = Convert.ToBase64String(bytesToEncode);
+
+            modelInfoRequest.SetRequestHeader("Authorization", "Basic " + encodedText);
+            yield return modelInfoRequest.SendWebRequest();
+
+            // Deserialize the response to a class
+            SDProgress sdp = JsonConvert.DeserializeObject<SDProgress>(modelInfoRequest.downloadHandler.text);
+            progress = sdp.progress;
+
+            EditorUtility.DisplayProgressBar("Generation in progress", progress * 100 + "%", progress);
+        }
+
+        _updateProgressRunning = null;
+        yield return null;
+    }
+    
     /// <summary>
     /// Find all the game objects that contains a certain component type.
     /// </summary>
